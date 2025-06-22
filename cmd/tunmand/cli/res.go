2bbc1ae3897861401ade18cf983ad3f38708904b
Philip Zingmark <philip.zingmark@gmail.com>
2025-06-22 18:03:34 +0200

    rm cluttered start log

--- cmd/tunmand/cli/res.go
+++ cmd/tunmand/cli/res.go
@@ -2,8 +2,6 @@ package cli
 
 import (
 	"fmt"
-	"runtime"
-	"time"
 )
 
 const (
@@ -14,15 +12,12 @@ _/  |_ __ __  ____   _____ _____    ____
  |  | |  |  /   |  \  Y Y  \/ __ \|   |  \
  |__| |____/|___|  /__|_|  (____  /___|  /
                  \/      \/     \/     \/ `
+)
+
+var (
 	version = "v0.0.1"
 )
 
 func startLog() {
 	fmt.Printf("%s\nVersion:\t\t\t%s\n", tundmand, version)
-	fmt.Println("======================================")
-	fmt.Printf("Start Time: %s\n", time.Now().Format(time.RFC1123))
-	fmt.Printf("Go Version: %s\n", runtime.Version())
-	fmt.Printf("OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
-	fmt.Printf("CPUs:       %d\n", runtime.NumCPU())
-	fmt.Println("======================================")
 }
